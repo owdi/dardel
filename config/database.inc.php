49d3ef9cba6e74cea629c532765948fd51be9ff5
olivier.dupre <o.dupre@hotmail.fr>
2015-10-11 22:28:08 +0200

    add some refactoring and theme in vendor

---
+++ config/database.inc.php
@@ -0,0 +1,6 @@
+<?php 
+
+//local
+define('server','mysql:host=127.0.0.1;dbname=dardelbdd');
+define('user','root');
+define('password','root');
--- system/constantes.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-define('plug_BDD_1','mysql:host=mysql51-45.perso;dbname=dardelbdd');
-define('plug_BDD_2','dardelbdd');
-define('plug_BDD_3','dardelbdd1');
-
- ?>
